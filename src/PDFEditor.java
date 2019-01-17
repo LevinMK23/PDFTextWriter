ea450c78a96e69f211a1f8c9a5ac2ce99cb698de
levinMK <levinmk23@gmail.com>
2019-01-17 17:51:11 +0300

    123

--- src/PDFEditor.java
+++ src/PDFEditor.java
@@ -12,7 +12,7 @@ public class PDFEditor {
     public static void main(String[] args) throws Exception {
 
         PdfReader reader = new PdfReader("/Users/levinMK/IdeaProjects/" +
-                "ImagePrinter/src/file.pdf"); // input PDF
+                "ImagePrinter/src/file.pdf");
         File names = new File("/Users/levinMK/IdeaProjects/" +
                 "ImagePrinter/src/names.txt");
         Scanner fin = new Scanner(names);
@@ -23,28 +23,23 @@ public class PDFEditor {
         ArrayList<InputStream> streams = new ArrayList<>();
         for (int j = 0; j < listNames.size(); j++){
             String text = listNames.get(j);
-            streams.add(new FileInputStream("/Users/levinMK/IdeaProjects/" +
-                    "ImagePrinter/src/output/file_output" + (j+1) + ".pdf"));
             PdfStamper stamper = new PdfStamper(reader,
                     new FileOutputStream("/Users/levinMK/IdeaProjects/" +
-                            "ImagePrinter/src/output/file_output" + (j+1) + ".pdf")); // output PDF
+                            "ImagePrinter/src/output/file_output" + (j+1) + ".pdf"));
             BaseFont bf = BaseFont.createFont(
                     "/Users/levinMK/IdeaProjects/ImagePrinter/src/tahoma/TAHOMA_0.TTF",
-                    BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED); // set font
-
+                    BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            streams.add(new FileInputStream("/Users/levinMK/IdeaProjects/" +
+                    "ImagePrinter/src/output/file_output" + (j+1) + ".pdf"));
             for (int i = 1; i <= reader.getNumberOfPages(); i++) {
                 PdfContentByte over = stamper.getOverContent(i);
 
                 over.beginText();
-                over.setFontAndSize(bf, 30);    // set font and size
-                over.setTextMatrix(130, 400);   // set x,y position (0,0 is at the bottom left)
+                over.setFontAndSize(bf, 30);
+                over.setTextMatrix(130, 400);
                 over.showText(text);  // set text
                 over.endText();
 
-//            over.setRGBColorStroke(0xFF, 0x00, 0x00);
-//            over.setLineWidth(5f);
-//            over.ellipse(250, 450, 350, 550);
-//            over.stroke();
             }
             stamper.close();
             reader = new PdfReader("/Users/levinMK/IdeaProjects/" +
